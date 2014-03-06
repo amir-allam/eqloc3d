743ee67d1417cd9a0064a0b18473287132e0e782
aallam <aallam@ucsd.edu>
2014-03-05 16:06:22 -0800

    1.0 - Updated Phalist Class

--- misc_tools.py
+++ misc_tools.py
@@ -389,32 +389,88 @@ class Stalist(list):
     #a class for each individual station. There is almost certainly a more elegant way to do this.
         def __init__(self,name,lat,lon,elev):
             self.name=name; self.lat=lat; self.lon=lon; self.elev=elev
-        def show(self): #show the contents of the station class
+        def show(self): #show the contents of the class
             print '%s %5.4f %5.4f %5.2f'% (self.name,self.lon,self.lat,self.elev)
 
 class Phalist(list):
 #A class containing the event information with arrival times for each event
-    def __init__(self,fnam):
+    def __init__(self,fnam,ftype='ANTDB'):
+        # fnam -    the file to be read
+        # ftype -   the file type; right now either 'SCEDC' or 'ANTDB'
+        if ftype is 'ANTDB' or ftype is 'antdb':
+            self.read_antdb(fnam)
+        elif ftype is 'SCEDC' or ftype is 'scedc':
+            self.read_scedc(fnam)
+
+    def read_antdb(self,fnam): #Read Antelope Database format (default)
+        ####DOES NOT WORK YET######
+        def __init__(self, db, evid): #Mal, this shouldn't take evid as an argument
+            with closing(dbopen(db, 'r')) as db:
+                view = db.schema_tables['origin']
+                view = view.subset('evid == %s' % evid)
+                view = view.subset('orid == prefor')
+                view = view.join('netmag', outer=True)
+                evid, time, lat, lon, depth, mag =  view.getv('evid', 'time', 'lat',
+                    'lon', 'depth', 'magnitude')
+                year = int(epoch2str(time, '%Y'))
+                month = int(epoch2str(time, '%m'))
+                day = int(epoch2str(time, '%d'))
+                hour = int(epoch2str(time, '%H'))
+                minute = int(epoch2str(time, '%M'))
+                second = float(epoch2str(time, '%S.%s'))
+                self.append({'id': evid, 'year': year, 'month': month, 'day': day,
+                    'hour': hour, 'min': minute, 'sec': second, 'lat': lat,
+                    'lon': lon, 'depth': depth, 'mag': mag, 'arrivals': []})
+                view = view.join('assoc')
+                view = view.join('arrival')
+                for record in view.iter_record():
+                    sta, arr_time, timeres, phase = record.getv('sta',
+                        'arrival.time', 'timeres', 'phase')
+                    ttime = arr_time - time
+                    self[-1]['arrivals'].append({'staname': sta, 'ttime': ttime,
+                        'qual': timeres, 'phase': phase}) 
+
+    def read_scedc(self,fnam): #Read SCEC Datacenter format
         #Read the file
         print 'Reading ' +fnam
         fid = open(fnam,'r')
         a = fid.readlines()
-        #Parse into a structure containing events
-        #self=list()
         ic = -1
-        for line in a:
+        for line in a: #Go line-by-line
             tmp=line.strip().split()
-            if tmp[0] is '#':
+            if tmp[0] is '#': #In SCEDC format, an event line begins with #
                 ic=ic+1
-                #Calculate epoch time
-                str_date=tmp[2]+'/'+tmp[3]+'/'+tmp[1]+' '+tmp[4]+':'+tmp[5]+':'+tmp[6]
-                ev_epoch=str2epoch(str_date)
-                self.append( {'ep':ev_epoch,'id':int(tmp[14]),'year':int(tmp[1]),'month':int(tmp[2]),'day':int(tmp[3]),'hour':int(tmp[4]),'min':int(tmp[5]),'sec':float(tmp[6]),'lon':float(tmp[8]),'lat':float(tmp[7]),'depth':float(tmp[9]),'mag':float(tmp[10]),'arrivals':list()} )
-            else:
-                #Calculate epoch time
-                pha_epoch=ev_epoch+float(tmp[1])
-                self[ic]['arrivals'].append( {'staname':tmp[0],'ttime':float(tmp[1]),'qual':float(tmp[2]),'phase':tmp[3],'epoch':pha_epoch} )
+                self.append(self.Event( int(tmp[14]),int(tmp[1]),int(tmp[2]),int(tmp[3]),int(tmp[4]),int(tmp[5]),float(tmp[6]),float(tmp[8]),float(tmp[7]),float(tmp[9]),float(tmp[10]) ))
+            else: #If it didn't begin with #, this line is a phase arrival for the last event
+                self[ic].arrivals.append( self.Arrival(tmp[0],float(tmp[1]),float(tmp[2]),tmp[3],self[ic].epoch ))
         print 'Finished reading '+fnam
+
+    class Event():
+    #A class containing event metadata (if it exists)
+        def __init__(self,evid,yr,mon,day,hr,mins,sec,lon,lat,dep,mag):
+            self.id=evid; #event id
+            self.year=yr; self.month=mon; self.day=day;  #event time
+            self.hour=hr; self.min=mins;  self.sec=sec;  #more event time
+            self.lon=lon; self.lat=lat;   self.depth=dep #event location
+            self.mag=mag; #event magnitude
+            #Calculate epoch time
+            str_date=str(mon)+'/'+str(day)+'/'+str(yr)+' '+str(hr)+':'+str(mins)+':'+str(sec)
+            print str_date
+            self.epoch=str2epoch(str_date) #time in seconds after 1/1/1970
+            self.arrivals=list() #This will be a list of phase arrivals
+        def show(self): #show the contents of the class in stdout
+            print '%u %u %u %u %u %u %5.4f %5.4f %5.4f %5.2f %5.2f %14.4f'% (self.id,self.year,self.month,self.day,self.hour,self.min,self.sec,self.lon,self.lat,self.depth,self.mag,self.epoch)
+            for pha in self.arrivals:
+                pha.show()
+
+    class Arrival():
+    #A class containing arrivals associated with an event; these will be entries in a
+    #   list under Event.arrivals
+        def __init__(self,staname,ttime,qual,phase,ev_epoch):
+            self.staname=staname; self.ttime=ttime; self.qual=qual; self.phase=phase
+            self.epoch=ev_epoch+ttime;
+        def show(self): #show the contents of the class in stdout
+            print '%s %5.4f %5.1f %s %14.4f'% (self.staname,self.ttime,self.qual,self.phase,self.epoch)
 
 class Traveltime_header_file():
 #This reads the header file passed as an argument by fnam, saves all of the header info to a class
